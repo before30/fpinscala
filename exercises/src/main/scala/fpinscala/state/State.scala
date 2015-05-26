bb25c3ad67bce1a98e5e0625f17a6175b4e9eafe
wonsuk.yang <before30@gmail.com>
2015-05-26 19:08:46 +0900

    * finish 6.7

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -40,17 +40,80 @@ object RNG {
     (i / (Int.MaxValue.toDouble + 1), r)
   }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def double2: Rand[Double] =
+    map(nonNegativeInt)(x => x / (Int.MaxValue.toDouble + 1))
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, r2) = int(rng)
+    val (d, r3) = double(r2)
+    ((i, d), r3)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, r2) = double(rng)
+    val (d2, r3) = double(r2)
+    val (d3, r4) = double(r3)
+    ((d1, d2, d3), r4)
+  }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def func(c: Int, acc: List[Int], r: RNG): (List[Int], RNG) = {
+      if (c == 0) (acc, r)
+      else {
+        val (i, nr) = int(r)
+        func( c - 1, i :: acc, nr)
+      }
+    }
+
+    func(count, Nil, rng)
+  }
+
+  def ints2(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(int))
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      println("ra " + ra + " rb " + rb)
+      val (v1, r2) = ra(rng)
+      val (v2, r3) = rb(r2)
+      (f(v1, v2), r3)
+    }
+  }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = map2(ra, rb)((_, _))
+
+  def sequence2[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    println("fs " + fs)
+    fs.foldRight(unit(List[A]()))((f, acc) => {println("f : " + f + " , acc " + acc); map2(f, acc)( _ :: _)})
+  }
+
+  def sequence3[A](fs: List[Rand[A]])(rng: RNG) = {
+    fs.foldRight(unit(List[A]()))((f, acc) => acc)
+  }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+
+    rng => {
+      def func(list: List[Rand[A]], r: RNG, acc: List[A]) : (List[A], RNG) = {
+        list match {
+          case Nil => (acc, r)
+          case h :: tail => {
+            val (v, r1) = h.apply(r)
+            func(tail, r1, v :: acc)
+          }
+        }
+      }
+
+      func(fs, rng, Nil)
+    }
+
+  }
 
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }
@@ -73,4 +136,22 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 object State {
   type Rand[A] = State[RNG, A]
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+}
+
+
+object Application extends App {
+
+//  println(RNG.int.apply(RNG.Simple(100)))
+//  println(RNG.double2(RNG.Simple(100)))
+//  println(RNG.ints(4)(RNG.Simple(100)))
+//  println(RNG.ints2(4)(RNG.Simple(100)))
+//
+//  println(RNG.intDouble(RNG.Simple(100)))
+//  println(RNG.both(RNG.int, RNG.double)(RNG.Simple(100)))
+//
+//  println(RNG.both(RNG.int, RNG.int)(RNG.Simple(100)))
+  println(RNG.map2(RNG.int, RNG.unit(List[Int](1, 2, 3)))(_ :: _)(RNG.Simple(100)))
+  println(RNG.sequence(List(RNG.int, RNG.int))(RNG.Simple(100)))
+  println(RNG.sequence2(List(RNG.int, RNG.int, RNG.int))(RNG.Simple(100)))
+  println(RNG.sequence3(List(RNG.int, RNG.int, RNG.int))(RNG.Simple(100)))
 }
